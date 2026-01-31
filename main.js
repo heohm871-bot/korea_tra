72b8adad56c6ad29b9ed0afec8b2ef035fdf2007
heohm871 <heohm871@gmail.com>
2026-01-31 03:32:26 +0000

    Update project structure with Firebase integration

--- main.js
+++ main.js
@@ -1,7 +1,1 @@
-import { initUser, uploadSampleData } from './firebase.js';
-
-// Initialize the user
-initUser();
-
-// To upload the sample data, uncomment the following line
-// uploadSampleData();
+// Add JS here
